e64b177c27d83d8959b06db843a2c09de395b735
webWizrd <superultradev000@gmail.com>
2017-03-21 20:12:46 +0100

    remove unnecessary comments

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import $ from 'jquery';
 import {Button, Panel, Form, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 
 class AddTask extends React.Component{
     render(){
        return(
          <Panel header="Add Task">
-            {/*<h1>Add task</h1>*/}
             <Form inline>
               <FormGroup>
                 <ControlLabel>Task Name</ControlLabel>
